9a2e307e589d1bc1993756fd871640e84e7b8ed9
Ingmar Jager <ingmarjager@gmail.com>
2020-03-30 20:37:59 +0200

    Add serial parse function that remembers leftovers

--- mcu.py
+++ mcu.py
@@ -32,6 +32,7 @@ class Microcontroller:
         self.crc = crcmod.predefined.mkPredefinedCrcFun('crc-16-usb')
         self.queue = queue
         self.connect()
+        self.serialdata = b''
 
     def _start_reader(self):
         """Start reader thread"""
@@ -86,54 +87,52 @@ class Microcontroller:
         self._send_buffer(SerialCommands.SensorData.format())
 
 
+    def parse_serial_data(self, data):
+        while len(data):
+            if data.startswith(b'###'):
+                i = data.index(b'\n')
+                print(data[3:i].decode("utf-8"))
+                # pop text and newline (so i+1)
+                data = data[i+1:]
+            elif data.startswith(SerialCommands.SensorData.format()):
+                # TODO: check data length, save data somewhere if not enough bytes have arrived.
+                #       Then also set timeout to discard data in case data never arrives
+                #       Also we should add the CRC16 to the sensor data
+
+                sensors_size = 4*4
+                offset = 4
+                end = offset+sensors_size
+                if len(data[offset:]) >= sensors_size:
+                    sensors = sensors_from_binary(data[offset:end])
+                    self.queue.put(sensors)
+                    data = data[end:]
+            elif data.startswith(SerialCommands.NewSettings.format()):
+                settings_size = 26
+                offset = 4
+                end = offset+settings_size
+                if len(data[offset:]) >= settings_size:
+                    settings = settings_from_binary(data[offset:end])
+                    self.queue.put(settings)
+                    data = data[end:]
+            else:
+                # # save data for next round
+                # self.serialdata.append(data)
+                # print('got unknown data:', data)
+                # data = []
+                break;
+        return data
+
     def _reader(self):
         """loop and copy serial->console"""
         try:
             while self._reader_alive:
                 # try to read up to 1kb at a time
-                data = self.serial.read(1024)
-                if data and len(data):
-                    while len(data):
-                        if data.startswith(b'###'):
-                            i = data.index(b'\n')
-                            print(data[3:i].decode("utf-8"))
-                            # pop text and newline (so i+1)
-                            data = data[i+1:]
-                        elif data.startswith(SerialCommands.SensorData.format()):
-                            # TODO: check data length, save data somewhere if not enough bytes have arrived.
-                            #       Then also set timeout to discard data in case data never arrives
-                            #       Also we should add the CRC16 to the sensor data
+                newdata = self.serial.read(1024)
+                if newdata and len(newdata):
+                    self.serialdata += newdata
+                    self.serialdata = self.parse_serial_data(self.serialdata)
 
-                            sensors_size = 4*4
-                            offset = 4
-                            end = offset+sensors_size
-                            sensors = sensors_from_binary(data[offset:end])
-                            self.queue.put(sensors)
-                            data = data[end:]
-                        elif data.startswith(SerialCommands.NewSettings.format()):
-                            settings_size = 26
-                            offset = 4
-                            end = offset+settings_size
-                            settings = settings_from_binary(data[offset:end])
-                            self.queue.put(settings)
-                            data = data[end:]
-                        else:
-                            print('got unknown data:', data)
-                            data = []
                 time.sleep(0.1)
-
-                    # else:
-                    #     self.queue.put(data)
-                    # print('got {} bytes back'.format(len(data)), type(data))
-                    # try:
-                    #     if chr(data[-1]) == '\n':
-                    #         text = ''.join([chr(c) for c in data])
-                    #         print(text)
-                    #     else:
-                    #         print("Got bytes: ", data)
-                    # except e:
-                    #     print('parse error:', e)
-                    #     pass
 
         except serial.SerialException:
             # ToDo how to handle
@@ -150,10 +149,12 @@ if __name__ == "__main__":
 
     BAUDRATE = 115200
     TTY = '/dev/cu.usbmodemC1DDCDF83'
-    conn = SerialConnection(port, baudrate)
+    queue = Queue()
+    mcu = Microcontroller(TTY, BAUDRATE, queue)
 
     s = Settings(
-         peep=20,
+        start=0,
+        peep=20,
         freq=20,
         ratio=2,
         pressure=40,
@@ -164,41 +165,39 @@ if __name__ == "__main__":
         min_tv=200,
         max_fio2=50,
         min_fio2=20)
-    message = s.get_bit_string()
-    print('sending settings: ', len(message), message)
-    # print('in ascii: ', binascii.hexlify(message))
-    conn.send_buffer(message)
+
+    mcu.led_on()
+    mcu.send_settings(s)
 
     time.sleep(1)
+    mcu.request_sensor_data()
 
-    req_sensor_data = 0x22226666
-    cmd = req_sensor_data.to_bytes(4, 'little')
-    conn.send_buffer(cmd)
     exit = False
 
     def exitFunc(_signal=None, _=None):
         global exit
-        conn.disconnect()
+        mcu.disconnect()
         exit = True
         print('bye')
 
     signal.signal(signal.SIGINT, exitFunc)
 
-
+    count = 0
+    toggle = 0
     while not exit:
+        if not queue.empty():
+            packet = queue.get()
+            print("Got packet:")
+            print(packet)
+
+        count += 1
+
+        if count > 10:
+            count -= 10
+            toggle += 1
+            if toggle % 2 == 0:
+                mcu.led_on()
+            else:
+                mcu.led_off()
+
         time.sleep(0.1)
-    # TTY = '/dev/cu.SLAB_USBtoUART'
-
-
-    # with serial.Serial(TTY, 115200, timeout=1) as ser:
-    #     ser.write(message)
-    #     thread = threading.Thread(target=read_from_port, args=(ser,))
-    #     thread.start()
-
-    # print('done, bye')
-
-    # time.sleep(10)
-    # thread.join()
-
-
-
