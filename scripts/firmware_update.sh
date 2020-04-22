4e92f8be7e3e52ceb0b7b33e9d01999de9eb9a66
Ingmar Jager <ingmarjager@gmail.com>
2020-04-22 13:32:53 +0200

    Update fw update script to retry 10 times when it fails

--- scripts/firmware_update.sh
+++ scripts/firmware_update.sh
@@ -3,15 +3,21 @@
 # cd to script dir
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-python mcu.py --bootloader --delay
+while true ; do
 
-echo "flash firmware over uart.."
-
-../bin/mxli -b 115200 -d /dev/ttyS0 -c12M -E ../bin/firmware.bin
-
-echo "done"
-
-python mcu.py --reset
+    python mcu.py --bootloader --delay
+    echo 'Start firmware update over uart..'
+    ../bin/mxli -b 115200 -d /dev/ttyS0 -c12M -E ../bin/firmware.bin
+    res=$?
+    python mcu.py --reset
+    if [ "$res" -eq 0 ]; then
+        echo 'done'
+        break
+    fi
+    ((c++)) && ((c==10)) && c=0 && break
+    echo 'Failed try $c. Try again'
+done
 
 # go back to previous directory
 cd -
+
