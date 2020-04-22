e91279e47cdd520a013921b0b681bea007051c9a
Ingmar Jager <ingmarjager@gmail.com>
2020-04-22 14:08:54 +0200

    Fix install script: works from anywhere now.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo "Copy systemd script to /etc/systemd/system/"
 
-sudo cp ./airone.service /etc/systemd/system/airone.service
+sudo cp $DIR/airone.service /etc/systemd/system/airone.service
 
 echo "Reload and enable service"
 sudo systemctl daemon-reload
